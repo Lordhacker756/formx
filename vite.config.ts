1332339f0911dec5e261603ba17766c8d4834007
Utkarsh Mishra <utkarshmishra2001@gmail.com>
2024-12-19 13:04:16 +0530

    FEAT: update build configuration and add type definitions support

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: ['src'],
+      outDir: 'dist/types',
+    })
+  ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'ReactFormValidationPackage',
-      fileName: (format) => `index.${format}.js`,
+      entry: path.resolve(__dirname, 'src/main.tsx'),
+      name: 'Formx',
+      fileName: (format) => {
+        if (format === 'umd') {
+          return 'index.umd.js';
+        } else if (format === 'es') {
+          return 'index.es.js';
+        } else {
+          return 'index.js'; // Add this line to generate the default index.js file
+        }
+      },
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
